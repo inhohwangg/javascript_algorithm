912660b71d5406b22e69c5f314fe3fbe117b40fb
inhohwangg <sk1440sk@naver.com>
2022-10-26 23:40:43 +0900

    commit

---
+++ class.js
@@ -0,0 +1,20 @@
+// 나의 풀이
+// 게임 캐릭터 클래스 만들기
+class Wizard {
+  constructor(health, mana, armor) {
+    this.health = health;
+    this.mana = mana;
+    this.armor = armor;
+  }
+  attack() {
+    return console.log("파이어볼");
+  }
+}
+
+const x = new Wizard(545, 210, 10);
+console.log(x.health, x.mana, x.armor);
+x.attack();
+
+// 출력
+// 545 210 10
+// 파이어볼

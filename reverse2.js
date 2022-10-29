f37028027eaa400e98131c50680642c8cee01e05
inhohwangg <sk1440sk@naver.com>
2022-10-29 23:09:47 +0900

    commit

---
+++ reverse2.js
@@ -0,0 +1,11 @@
+// const n = [1, 2, 3, 4, 5];
+// console.log(n.reverse());
+
+// const n2 = [2, 4, 6, 7, 8];
+// console.log(n2.reverse());
+
+const n3 = "1 2 3 4 5";
+console.log(n3.split(" ").reverse().join(" "));
+
+const n4 = "2 4 6 7 8";
+console.log(n4.split(" ").reverse().join(" "));
--- wordcreate.js
+++ wordcreate.js
@@ -1,4 +1,5 @@
 let str = "안녕하세요. 저는 제주대학교 컴퓨터공학전공 인호입니다.";
 let result = str.split(" ").length;
-
+let result2 = str.split(" ").join("");
 console.log(result);
+console.log(result2);
